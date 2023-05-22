a31a48ee38b39be1029ac19e4765bebead4c3460
Ali Mehraji <a.mehraji75@gmail.com>
2023-05-22 22:51:42 +0330

    Update: Description For Image_Prune.sh

--- Docker-Related/Image_Prune.sh
+++ Docker-Related/Image_Prune.sh
@@ -1,4 +1,27 @@
-#! /bin/bash 
+#! /bin/bash
+
+# =================================================================
+
+# Author: Ali Mehraji
+# Email: a.mehraji75@gmail.com
+
+# Script Name: Image_Prune.sh
+# Date Created: Mon May 22 10:47:06 PM +0330 2023
+# Last Modified: Mon May 22 10:47:06 PM +0330 2023
+
+# Description:
+# This script is for keeping last 2 image that built and tagged via different tags 
+
+# Usage:
+# You Can use this script like below:
+# 1. bash Image_Prune.sh
+# 2. First grant executable permission via chmod +x Image_Prune.sh and invoke it ./Image_Prune.sh
+
+# =================================================================
+
+# ======================== Start Of Code ==========================
+
+set -e
 
 # keep last 2 builds for each image from the repository
 _KEEP_TWO()
